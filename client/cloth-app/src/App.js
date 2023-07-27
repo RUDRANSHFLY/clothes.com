5aa08e4eb54282b59dceec5565642690b65e597e
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-07-27 10:24:30 +0530

    minor changes extra things removed

--- client/cloth-app/src/App.js
+++ client/cloth-app/src/App.js
@@ -1,35 +1,3 @@
-import 
-    {
-        createBrowserRouter,
-        RouterProvider,
-
-    } from 'react-router-dom';
-
-    import Home from './pages/Home/Home.jsx';
-    import Product from './pages/Product/Product.jsx';
-    import Products from './pages/Products/Products.jsx';
-
-
-const router = createBrowserRouter(
-    [
-        {
-            path:"/",
-            element: <Home/>
-        },
-        
-        {
-            path:"/product/:id",
-            element: <Product/>
-        },
-
-        {
-            path:"/products/:id",
-            element: <Products/>
-        },
-    ]
-)
-
-
 function App() {
     return (
         <div>
