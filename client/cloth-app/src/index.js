5b34e05c2eb5d94ed76013e75da51648eaa5c065
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-07-27 08:33:10 +0530

    main index file of cloth-app

---
+++ client/cloth-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM, { createRoot }  from 'react-dom/client';
+import App from './App.js';
+
+const app = document.getElementById("root");
+const root = createRoot(app);
+
+root.render(
+   <App/>
+);
