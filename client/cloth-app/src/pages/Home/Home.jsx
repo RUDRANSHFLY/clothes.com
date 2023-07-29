8e2d4fd0705498894f1bf5c31c4f226763b06899
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-07-29 13:14:17 +0530

    ui component added

--- client/cloth-app/src/pages/Home/Home.jsx
+++ client/cloth-app/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Slider from '../../components/Slider/Slider.jsx';
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts.jsx';
 const Home = () => {
   return (
     <div>
        <Slider/>
+       <FeaturedProducts type='featured'/>
     </div>
   )
 };
