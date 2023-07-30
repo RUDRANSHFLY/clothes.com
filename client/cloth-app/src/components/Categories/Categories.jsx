749dd4dfa9e8c0414bc37bfe1b1e52a5ebd783dd
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-07-30 15:23:56 +0530

    minor changes

--- client/cloth-app/src/components/Categories/Categories.jsx
+++ client/cloth-app/src/components/Categories/Categories.jsx
@@ -1,39 +1,75 @@
 import React from 'react'
 import "../Categories/Categories.scss"
+import { Link } from 'react-router-dom'
+
+
 const Categories = () => {
   return (
     <div className='categories'>
         <div className="col">
             <div className="row">
-                    r1
+                    <img src="https://images.pexels.com/photos/8638311/pexels-photo-8638311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+                    <Link className='link' to='/products/3'>
+                        <button>
+                            SALE
+                        </button>
+                    </Link>
             </div>
+
             <div className="row">
-                    r2
+                    <img src="https://images.pexels.com/photos/2690323/pexels-photo-2690323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+                    
+                    <Link className='link' to='/products/2'>
+                        <button>
+                            WOMEN
+                        </button>
+                    </Link>
             </div>
         </div>
         <div className="col">
             <div className="row">
-                r3
+                    <img src="https://images.pexels.com/photos/1755385/pexels-photo-1755385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+                    
+                    <Link className='link' to='/products/2'>
+                        <button>
+                            NEW SEASON
+                        </button>
+                    </Link>
             </div>
         </div>
         <div className="col col-l">
             <div className="row">
                 <div className="col">
                     <div className="row">
-                        r4
+                        <img src="https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+                        
+                        <Link className='link' to='/products/2'>
+                            <button>
+                                MEN
+                            </button>
+                    </Link>
                     </div>
                 </div>
                 <div className="col">
                     <div className="row">
-                        r5
+                    <img src="https://images.pexels.com/photos/11158679/pexels-photo-11158679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+                    <Link className='link' to='/products/2'>
+                        <button>ACCESORIES</button>
+                    </Link>
                     </div>
                 </div>
-                <div className="row"></div>
+                
             </div>
+                <div className="row">
+                <img src="https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+                    <Link className='link' to='/products/3'>
+                        <button>SHOES</button>
+                    </Link>
+                </div>
 
         </div>
     </div>
   )
 }
 
-export default Categories
+export default Categories;
