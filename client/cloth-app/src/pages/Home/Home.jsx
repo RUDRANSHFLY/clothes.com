7e5536b55b0e48309412e548277bb56c11e25936
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-07-30 08:31:42 +0530

    Trending Products Added

--- client/cloth-app/src/pages/Home/Home.jsx
+++ client/cloth-app/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Slider from '../../components/Slider/Slider.jsx';
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts.jsx';
+import TrendingProducts from '../../components/TrendingProducts/TrendingProducts.jsx';
 const Home = () => {
   return (
     <div>
        <Slider/>
        <FeaturedProducts type='featured'/>
+       <TrendingProducts type='Trending'/>
     </div>
   )
 };
