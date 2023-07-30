0d9c661a92b341fcdd80cff16200929cea9ce6b4
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-07-30 15:25:18 +0530

    category componet asdded

--- client/cloth-app/src/pages/Home/Home.jsx
+++ client/cloth-app/src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Slider from '../../components/Slider/Slider.jsx';
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts.jsx';
 import TrendingProducts from '../../components/TrendingProducts/TrendingProducts.jsx';
+import Categories from '../../components/Categories/Categories.jsx';
+import Contact from '../../components/Contact/Contact.jsx';
 const Home = () => {
   return (
     <div>
        <Slider/>
        <FeaturedProducts type='featured'/>
+       <Categories/>
        <TrendingProducts type='Trending'/>
+       <Contact/>
     </div>
   )
 };
