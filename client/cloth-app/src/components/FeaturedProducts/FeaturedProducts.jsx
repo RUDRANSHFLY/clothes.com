e22512d5fa3aceb1bce3a3b33f76d643c8260a50
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-08-06 12:28:03 +0530

    edited something

--- client/cloth-app/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/cloth-app/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import "../FeaturedProducts/FeaturedProducts.scss";
 import Card from "../Card/Card.jsx";
-import { 
-    useEffect,
-    useState ,
-} from "react";
-import axios  from "axios";
+import useFetch from "../../hooks/useFetch.js";
+import Loading from "../Loading/Loading.jsx";
 
 const FeaturedProducts = ({ type }) => {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     img: "https://images.pexels.com/photos/7872818/pexels-photo-7872818.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //     img2: "https://images.pexels.com/photos/7872814/pexels-photo-7872814.jpeg?auto=compress&cs=tinysrgb&w=600",
-  //     title: "Coat Opens",
-  //     isNew: false,
-  //     oldPrice: 699,
-  //     newPrice: 499,
-  //   },
+
+        const {data,loading,error} = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`);
 
   //   {
   //     id: 2,
@@ -49,32 +38,12 @@ const FeaturedProducts = ({ type }) => {
   //     newPrice: 1500,
   //   },
   // ];
+
   
- const [data, setdata] = useState([]);
-  
-  useEffect(() => {
-        const fetchData = async ()=> {
-            try {
-                const res = await axios.get(process.env.REACT_APP_API_URL+`/products?populate=*&[filters] [type] [$eq]=${type}`,{
-                   headers : {
-                    authorization : "bearer "+process.env.REACT_APP_API_TOKEN,
-                   },
-                });
-                console.log(res.data.data);
-               setdata(res.data.data);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchData();    
-  }, []);
+ 
   
   return (
-
-
-
-
-    <div className="featuredProducts">
+  <div className="featuredProducts">
       <div className="top">
         <h1>{type} Products</h1>
         <p>
@@ -86,9 +55,11 @@ const FeaturedProducts = ({ type }) => {
       </div>
 
       <div className="bottom">
-        {data.map((i) => (
-          <Card item={i} key={i.id} />
-        ))}
+        {
+          loading ? Loading : data.map((i) => (
+                                  <Card item={i} key={i.id} />
+                              ))
+        }
       </div>
     </div>
   );
---
+++ client/cloth-app/src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Loading/Loading.scss"
+
+const Loading = () => {
+  return (
+    <div>Loading</div>
+  )
+}
+
+export default Loading;
