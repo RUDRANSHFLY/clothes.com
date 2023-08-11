f2296fc7ed871bbd7157b8fdd5dc4f6e5941851b
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-08-11 07:03:26 +0530

    new collection usecloth added

---
+++ server/src/api/usercloth/routes/usercloth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * usercloth router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::usercloth.usercloth');
---
+++ server/src/api/usercloth/services/usercloth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * usercloth service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::usercloth.usercloth');
