6fabc492097838a3fc22f6ebc2d7818b58e9675b
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-09-03 10:39:41 +0530

    Error Page Added

---
+++ client/cloth-app/src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Error/Error.scss";
+
+const Error = () => {
+  return (
+    <div className='error'>
+     <img src="https://cdn.dribbble.com/users/4672508/screenshots/11584848/media/bf99e1c02486a053bffcf88366923dae.jpg?resize=1000x750&vertical=center" alt="" srcset="" />
+    </div>
+  )
+}
+
+export default Error;
