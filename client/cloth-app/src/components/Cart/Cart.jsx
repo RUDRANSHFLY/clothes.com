f26c1f39463a29aa31e8cac660d2c7e533a98285
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-09-04 06:23:12 +0530

    Media Query

--- client/cloth-app/src/components/Cart/Cart.jsx
+++ client/cloth-app/src/components/Cart/Cart.jsx
@@ -1,49 +1,50 @@
-import React from 'react'
-import "../Cart/Cart.scss"
+import React from 'react';
+import "../Cart/Cart.scss";
+import "../Cart/CartUtil.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import { useDispatch } from "react-redux";
 import { removeItem, resetCart } from '../../redux/cartReducer.js';
 
 
 
 const Cart = () => {
-  
+
   const products = useSelector(state => state.cart.products);
   const dispatch = useDispatch();
- 
+
   const totalPrice = () => {
-    let total = 0 ;
+    let total = 0;
     products.forEach(i => {
-        (total += i.quantity * i.price )
+      (total += i.quantity * i.price)
     });
     return total.toFixed(2);
   }
   return (
     <div className='cart'>
       <h1>Products in your Cart</h1>
-        {products?.map(item => (
+      {products?.map(item => (
         <div className="item" key={item.id} >
-          <img src={process.env.REACT_APP_UPLOAD_URL+item.img} alt="" />
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
           <div className="details">
-            <h1>{item?.title.substring(0,10)}</h1>
-            <p>{item?.desc?.substring(0,10)}</p>
+            <h1>{item?.title.substring(0, 10)}</h1>
+            <p>{item?.desc?.substring(0, 10)}</p>
             <div className="itemPrice">
               {item.quantity} * {item.price}
             </div>
           </div>
-          <FontAwesomeIcon icon={faTrashCan} className='delete' onClick={()=>dispatch(removeItem(item.id))}/>
+          <FontAwesomeIcon icon={faTrashCan} className='delete' onClick={() => dispatch(removeItem(item.id))} />
         </div>
 
       ))}
       <div className="total">
-          <span>SUBTOTAL</span>
-          <span> $ {totalPrice()}</span>
+        <span>SUBTOTAL</span>
+        <span> $ {totalPrice()}</span>
       </div>
       <div className="but">
-      <button>Checkout</button>
-      <button id="reset" onClick={() => dispatch(resetCart()) }>Reset Cart</button>
+        <button>Checkout</button>
+        <button id="reset" onClick={() => dispatch(resetCart())}>Reset Cart</button>
 
       </div>
     </div>
