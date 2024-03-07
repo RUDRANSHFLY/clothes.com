06196945a2894dade42ddc527cb3fa368a61cd53
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-07 15:30:45 +0530

    feat: Added dynamic filtering and sorting to List component

--- client/cloth-app/src/components/List/List.jsx
+++ client/cloth-app/src/components/List/List.jsx
@@ -6,23 +6,29 @@ import "../List/List.scss"
 
 
 const List = ({ catId, maxPrice, sortPrice, subCat }) => {
-  
- 
+
+
+  // const { data, loading, error } = useFetch(
+  //   `/products?populate=*&[filters][categories][id][$eq]=${catId}`
+
+  // );
+
   const { data, loading, error } = useFetch(
     `/products?populate=*&[filters][categories][id][$eq]=${catId}${subCat.map(
-      (item) => `&[filters][sub_categories][id][$eq]=${item}`
-    )}&[filters][price][$lte]=${maxPrice}${sortPrice ? `&sort=price:${sortPrice}` : ' '}`
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`)}&[filters][price][$lte]=${maxPrice}${sortPrice ? `&sort=price:${sortPrice}` : ' '}`
   );
+
+  console.log(catId, maxPrice, sortPrice, subCat);
 
   return (
     <div className='list'>
       {loading
-        ? <Loading/>
+        ? <Loading />
         : data?.map((item) => (
           <Card item={item} key={item.id} />
         ))}
     </div>
-  )
+  );
 }
 
 export default List;
